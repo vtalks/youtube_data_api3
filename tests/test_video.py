563fadeede147eac9b587741b30360e7d1a61c5d
Raül Pérez <repejota@gmail.com>
2018-06-19 12:47:01 +0200

    Fix tests

--- tests/test_video.py
+++ tests/test_video.py
@@ -6,7 +6,7 @@ from youtube_data_api3 import video
 class VideoTest(unittest.TestCase):
 
     def test_video_code(self):
-        url = "https://www.youtube.com/watch?v=5UG57xQL_RE&t="
-        expected_code = "5UG57xQL_RE&t="
+        url = "https://www.youtube.com/watch?v=5UG57xQL_RE"
+        expected_code = "5UG57xQL_RE"
         video_code = video.get_video_code(url)
         self.assertEqual(expected_code, video_code)
