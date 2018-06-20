37b04e178eedecc2dbccf4af6ded61685ebeb5e0
Raül Pérez <repejota@gmail.com>
2018-06-20 20:14:25 +0200

    moar tests

--- tests/test_playlist.py
+++ tests/test_playlist.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import patch
+import requests_mock
 
 from youtube_data_api3 import playlist
 
@@ -19,18 +19,31 @@ class PlaylistTest(unittest.TestCase):
 
         self.assertTrue('Invalid url "invalid_url"' in str(context.exception))
 
-    @patch("youtube_data_api3.playlist.fetch_playlist_data")
-    def test_fetch_playlist_data(self, fake_fetch_playlist_data):
-        # mock fetch playlist data
-        fake_fetch_playlist_data.return_value = {
-            "id": "fake_playlist_id",
-            "snippet": {
-                "title": "playlist title",
-                "description": "playlist description",
-                "publishedAt": "2012-10-01T15:27:35.000Z",
+    def test_fetch_playlist_data(self):
+        with requests_mock.Mocker() as m:
+            fake_playlist_video_data = {
+              "kind": "youtube#playlistListResponse",
+              "etag": "etag",
+              "nextPageToken": "next_page_token",
+              "prevPageToken": "prev_page_token",
+              "pageInfo": {
+                "totalResults": 10,
+                "resultsPerPage": 10,
+              },
+              "items": [
+                  {
+                      "id": "fake_playlist_id",
+                      "snippet": {
+                          "title": "playlist title",
+                          "description": "playlist description",
+                          "publishedAt": "2012-10-01T15:27:35.000Z",
+                      }
+                  },
+              ]
             }
-        }
+            m.get('https://www.googleapis.com/youtube/v3/playlists', json=fake_playlist_video_data)
 
-        data = playlist.fetch_playlist_data("youtube_api_key", "fake_playlist_id")
+            data = playlist.fetch_playlist_data("youtube_api_key", "fake_playlist_id")
 
-        self.assertEqual(data["id"], fake_fetch_playlist_data.return_value["id"])
+            self.assertEqual(data["id"], fake_playlist_video_data["items"][0]["id"])
+
